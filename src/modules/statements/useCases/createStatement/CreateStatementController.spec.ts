68eded30d302cf9acaba50196764c2d3788efb91
IanaCris <ianasousati@gmail.com>
2021-08-18 00:40:27 -0300

    test integration

---
+++ src/modules/statements/useCases/createStatement/CreateStatementController.spec.ts
@@ -0,0 +1,14 @@
+import { app } from "../../../../app";
+import request from "supertest";
+
+describe("Create Statement Controller", () => {
+  it("should be able create a new statement", async () => {
+    const response = await request(app).post("/statement/deposit")
+      .send({
+        amount: 15,
+        description: "Compra tal"
+      });
+
+    console.log(response.status);
+  });
+});
