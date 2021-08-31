6f5207415d9a6099c17d3066396661cbcebe2f4a
IanaCris <ianasousati@gmail.com>
2021-08-30 23:34:04 -0300

    routes transfer statement

--- src/modules/statements/useCases/transferStatement/TransferStatementController.ts
+++ src/modules/statements/useCases/transferStatement/TransferStatementController.ts
@@ -4,13 +4,18 @@ import { TransferStatementUseCase } from "./TransferStatementUseCase";
 
 class TransferStatementController {
   async execute (request: Request, response: Response) {
-    const { id: user_id } = request.user;
-    const { receiver_user_id } = request.query;
+    const { id: send_user_id } = request.user;
+    const { user_id } = request.params;
     const { amount, description } = request.body;
 
     const transferStatementUseCase = container.resolve(TransferStatementUseCase);
 
-    const transferStatement = await transferStatementUseCase.execute();
+    const transferStatement = await transferStatementUseCase.execute({
+      amount,
+      description,
+      send_user_id,
+      user_id
+    });
 
     return response.json(transferStatement);
   }
--- src/modules/statements/useCases/transferStatement/TransferStatementUseCase.ts
+++ src/modules/statements/useCases/transferStatement/TransferStatementUseCase.ts
@@ -1,6 +1,12 @@
 import { inject, injectable } from "tsyringe";
 import { IStatementsRepository } from "../../repositories/IStatementsRepository";
 
+interface IRequest {
+  send_user_id: string;
+  user_id: string;
+  amount: number;
+  description: string;
+}
 @injectable()
 class TransferStatementUseCase {
   constructor(
@@ -8,7 +14,7 @@ class TransferStatementUseCase {
     private statementsRepository: IStatementsRepository
   ) {}
 
-  async execute() {
+  async execute({ amount, description, send_user_id, user_id}: IRequest) {
 
   }
 }
